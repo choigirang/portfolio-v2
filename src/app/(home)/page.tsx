ab2a42cbb79fdae2dc33d313ce7d72349268728c
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-04 20:54:15 +0900

    default page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Main from "./main";
+import About from "../(about)/about";
+
+/** 2024/04/04 - default page*/
+export default function Home() {
+  return (
+    <>
+      <Main />
+      <About />
+    </>
+  );
+}
