699706ceb901f9ebb59c133ee125d94a7592b46a
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-04 20:53:50 +0900

    create main & add style

---
+++ src/app/(home)/main.tsx
@@ -0,0 +1,8 @@
+export default function Main() {
+  return (
+    /** 2024/04/04 - error padding default 설정하기 */
+    <main className="w-full h-[800px] bg-red-100 py-0 px-default">
+      여기는 Main입니다.
+    </main>
+  );
+}
