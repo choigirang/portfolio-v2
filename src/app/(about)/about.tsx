d5a8edf9a5ecef3ec48cb50a24acf7a326306d82
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-05 22:07:56 +0900

    feat: about section, display info

--- src/app/(about)/about.tsx
+++ src/app/(about)/about.tsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
+import { meData } from "@/assets/aboutMe";
+
 export default function About() {
   return (
-    <article id="about">
-      <section></section>
+    <article className="flex justify-center w-full h-[800px]">
+      <section className="container">
+        <ul className="w-full grid place-items-start gap-y-16 grid-cols-fluidity">
+          {meData.map(({ displayName, displayTxt, Icon, link }) => (
+            <li key={displayName} className="flex space-x-4 ml-0 sm:ml-16">
+              {<Icon className="w-16 h-16" />}
+              <div>
+                <span>{displayName}</span>
+                {link && (
+                  <Link href={link} target="_blank">
+                    {displayTxt}
+                  </Link>
+                )}
+                {!link && <span>{displayTxt}</span>}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
     </article>
   );
 }
