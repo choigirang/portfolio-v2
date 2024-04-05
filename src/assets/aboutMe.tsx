0be2df574d16f2e55453d6d686b2f2d71840a49d
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-05 22:09:49 +0900

    feat: create my info data

--- src/assets/aboutMe.tsx
+++ src/assets/aboutMe.tsx
@@ -1,5 +1,57 @@
+import {
+  CalendarDaysIcon,
+  CodeBracketIcon,
+  FlagIcon,
+  IdentificationIcon,
+  MapPinIcon,
+  PencilIcon,
+  PhoneIcon,
+} from "@heroicons/react/16/solid";
+
 interface AboutMe {
-  dispalyName: string;
+  displayName: string;
   displayTxt: string;
-  // icon: typeof
+  Icon: typeof IdentificationIcon;
+  link?: string;
 }
+
+export const meData: AboutMe[] = [
+  {
+    displayName: "이름",
+    displayTxt: "최기랑",
+    Icon: IdentificationIcon,
+  },
+  {
+    displayName: "생년월일",
+    displayTxt: "1996.07.27",
+    Icon: CalendarDaysIcon,
+  },
+  {
+    displayName: "주소지",
+    displayTxt: "인천 남동구",
+    Icon: MapPinIcon,
+  },
+  {
+    displayName: "연락처",
+    displayTxt: "010-7255-7942",
+    Icon: PhoneIcon,
+    link: "tel:010-7255-7942",
+  },
+  {
+    displayName: "학력",
+    displayTxt: "한양사이버대학교 - 미디어디자인",
+    Icon: FlagIcon,
+  },
+  {
+    displayName: "깃허브",
+    displayTxt: "https://github.com/choigirang",
+    Icon: CodeBracketIcon,
+    link: "https://github.com/choigirang",
+  },
+  {
+    displayName: "블로그",
+    displayTxt: "https://choigirang.github.io/",
+    Icon: PencilIcon,
+    link: "https://choigirang.github.io/",
+  },
+];
