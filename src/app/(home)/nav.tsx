585afc24313b7b054ef4e2bc6f73cbbfc94ee34e
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-05 22:08:53 +0900

    feat: server / client (nav)

--- src/app/(home)/nav.tsx
+++ src/app/(home)/nav.tsx
@@ -1,20 +1,17 @@
 import Link from "next/link";
+import Category from "./category";
 
 /** 2024/04/04 - 레이아웃 header 컴포넌트 */
 export default function Header() {
   return (
-    <nav className="fixed top-0 left-0 w-full h-32 px-[calc((100%-1200px)/2)] py-0 flex justify-center bg-transparent">
-      <div className="w-full flex justify-between items-center">
+    <nav className="nav">
+      <div className="container h-full flex justify-between items-center pt-0">
         <h1 className="text-3xl text-yellow-400 font-bold">
           <Link href="/" title="최기랑 포트폴리오">
             My Portfolio.
           </Link>
         </h1>
-        <ul className="flex gap-10">
-          <li className="mx-2 hover:cursor-pointer">About</li>
-          <li className="mx-2 hover:cursor-pointer">Skills</li>
-          <li className="mx-2 hover:cursor-pointer">Projects</li>
-        </ul>
+        <Category />
       </div>
     </nav>
   );
