a7257b240c1a28b2921220b09efd9ae214a27d68
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-08 23:36:19 +0900

    feat: skills server components

--- src/app/(skills)/skills.tsx
+++ src/app/(skills)/skills.tsx
@@ -1,3 +1,12 @@
+import { tools } from "@/assets/skills";
+import Lists from "./lists";
+
 export default function Skills() {
-  return <div id="skills" className="w-full h-[800px] bg-black"></div>;
+  return (
+    <article id="skills" className="w-full h-[600px] flex justify-center">
+      <section className="container h-[600px]">
+        <Lists data={tools} title={"사용 가능한 언어 & 도구"} />
+      </section>
+    </article>
+  );
 }
