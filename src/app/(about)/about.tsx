6f949866839f54152e25bf43632cc000c8ea1a50
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-08 23:35:28 +0900

    feat: about page info & style

--- src/app/(about)/about.tsx
+++ src/app/(about)/about.tsx
@@ -3,20 +3,30 @@ import { meData } from "@/assets/aboutMe";
 
 export default function About() {
   return (
-    <article className="flex justify-center w-full h-[800px]">
-      <section className="container">
-        <ul className="w-full grid place-items-start gap-y-16 grid-cols-fluidity">
+    <article className="flex justify-center w-[80%] h-[400px] ">
+      <section className="container h-[400px]">
+        <ul className="grid place-items-start gap-y-16 grid-cols-fluidity grid-rows-fluidity">
           {meData.map(({ displayName, displayTxt, Icon, link }) => (
-            <li key={displayName} className="flex space-x-4 ml-0 sm:ml-16">
+            <li
+              key={displayName}
+              className="flex justify-between space-x-4 px-4"
+            >
               {<Icon className="w-16 h-16" />}
-              <div>
-                <span>{displayName}</span>
-                {link && (
-                  <Link href={link} target="_blank">
+              <div className="relative top-3 flex flex-col whitespace-nowrap space-y-1.5 text-left">
+                <span className="font-bold">{displayName}</span>
+                {link ? (
+                  <Link
+                    href={link}
+                    target="_blank"
+                    className="
+                    font-black text-sm transition-colors hover:underline underline-offset-4
+                    text-yellow-500 focus:text-yellow-600 active:text-yellow-600"
+                  >
                     {displayTxt}
                   </Link>
+                ) : (
+                  <span className="">{displayTxt}</span>
                 )}
-                {!link && <span>{displayTxt}</span>}
               </div>
             </li>
           ))}
