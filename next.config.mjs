c1b00adc9f517ad39f6e1a0f9ab98ff3b8aa989d
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-10 22:58:05 +0900

    feat: add image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // cdn으로 svg 가져오는 경우 사용
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.simpleicons.org",
+      },
+      {
+        protocol: "https",
+        hostname: "choigirang-portfolio.s3.ap-northeast-2.amazonaws.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
