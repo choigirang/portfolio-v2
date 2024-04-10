8ae9fae8646c537beb268216e82749412b79afea
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-10 23:00:37 +0900

    feat: skills 목록에 따른 컴포넌트 추가

--- src/app/(skills)/skills.tsx
+++ src/app/(skills)/skills.tsx
@@ -1,11 +1,14 @@
-import { tools } from "@/assets/skills";
+import { beSkills, feSkills, etcSkills, tools } from "@/assets/skills";
 import Lists from "./lists";
 
 export default function Skills() {
   return (
-    <article id="skills" className="w-full h-[600px] flex justify-center">
-      <section className="container h-[600px]">
-        <Lists data={tools} title={"사용 가능한 언어 & 도구"} />
+    <article id="skills" className="w-full flex justify-center">
+      <section className="container h-full flex-col justify-center gap-6 pt-0">
+        <Lists data={tools} title="사용 가능한 언어 & 도구" />
+        <Lists data={feSkills} title="Frontend측 기술" />
+        <Lists data={beSkills} title="Backend측 기술" />
+        <Lists data={etcSkills} title="사용해본 경험이 있는" />
       </section>
     </article>
   );
