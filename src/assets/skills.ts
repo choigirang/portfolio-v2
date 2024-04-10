b3c141389a2b39cb50f412fc5764bf5dd8ca313e
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-10 23:01:03 +0900

    chore: skills info edit

--- src/assets/skills.ts
+++ src/assets/skills.ts
@@ -38,7 +38,7 @@ export const tools: MySkills[] = [
   },
   {
     name: "figma",
-    color: "#181717",
+    color: "#F24D1D",
     displayName: "Figma",
     description: `다양한 직군들의 원활한 소통을 위해 사용합니다.`,
     link: `https://1-blue.github.io/posts/Git-GitHub`,
@@ -62,6 +62,27 @@ export const feSkills: MySkills[] = [
     link: "https://velog.io/@1-blue/CSR-SSR-SSG-ISR-%EA%B0%9C%EB%85%90-%EC%A0%95%EB%A6%AC-Next.js/",
   },
   {
+    name: "reactquery",
+    color: "#FF4154",
+    displayName: "React-Query",
+    description: `서버의 요청할 CRUD에 대한 훅을 만들 수 있고, 서버의 데이터를 관리하기 위해 사용할 수 있습니다.`,
+    link: "https://1-blue.github.io/posts/react-query",
+  },
+  {
+    name: "redux",
+    color: "#764ABC",
+    displayName: "Redux / RTK",
+    description: `Flux와 Redux의 동작 흐름에 대해 이해하고 있고, Redux-Toolkit과 TypeScript를 활용한 코드를 구현할 수 있습니다.`,
+    link: "https://1-blue.github.io/posts/Redux",
+  },
+
+  {
+    name: "recoil",
+    color: "#3578E5",
+    displayName: "Recoil",
+    description: `간단한 상태관리를 위해 사용해본 경험이 있습니다.`,
+  },
+  {
     name: "tailwindcss",
     color: "#06B6D4",
     displayName: "TailwindCss",
@@ -74,26 +95,6 @@ export const feSkills: MySkills[] = [
     description: `전역 스타일 설정을 할 수 있고, TypeScript를 적용해서 코드를 구현할 수 있습니다.`,
     link: "https://1-blue.github.io/posts/styled-components",
   },
-  {
-    name: "redux",
-    color: "#764ABC",
-    displayName: "Redux / Redux-Toolkit",
-    description: `Flux와 Redux의 동작 흐름에 대해 이해하고 있고, Redux-Toolkit과 TypeScript를 활용한 코드를 구현할 수 있습니다.`,
-    link: "https://1-blue.github.io/posts/Redux",
-  },
-  {
-    name: "reactquery",
-    color: "#FF4154",
-    displayName: "React-Query / Tanstack-Query",
-    description: `서버의 요청할 CRUD에 대한 훅을 만들 수 있고, 서버의 데이터를 관리하기 위해 사용할 수 있습니다.`,
-    link: "https://1-blue.github.io/posts/react-query",
-  },
-  {
-    name: "recoil",
-    color: "#3578E5",
-    displayName: "Recoil",
-    description: `간단한 상태관리를 위해 사용해본 경험이 있습니다.`,
-  },
 ];
 
 /** 2023/08/14 - 사용 가능한 BE측 기술들 - by 1-blue */
@@ -103,55 +104,49 @@ export const beSkills: MySkills[] = [
     color: "#000000",
     displayName: "Express",
     description: `Request, Response, Middleware, Router에 대한 이해를 하고 있고, 인증 및 CRUD를 위한 서버를 만들 수 있습니다.`,
-    link: "https://1-blue.github.io/posts/Setting-NodeJs",
+    link: "https://choigirang.github.io/posts/1-Node-Express-(1)/",
+  },
+  {
+    name: "mongodb",
+    color: "#53B145",
+    displayName: "MongDB",
+    description: `MongoDB를 활용하여 데이터에 대한 모델링을 구현하고 데이터를 관리해본 경험이 있습니다.`,
+    link: "https://choigirang.github.io/posts/MongoDB-%EB%AC%B8%EB%B2%95/",
+  },
+  {
+    name: "amazonec2",
+    color: "#53B145",
+    displayName: "EC2",
+    description: `https / SSL 인증 및 로드 밸런서를 적용하여 개인 프로젝트 배포를 해본 경험이 있습니다.`,
+    link: "https://choigirang.github.io/posts/AWS-EC2-2%EC%9E%A5/",
   },
   {
     name: "amazons3",
     color: "#569A31",
     displayName: "AWS-S3",
-    description: `버킷을 생성하고 접근 권한을 열고 이미지를 저장할 수 있습니다.\n또한 presignedURL을 이용해 안전하게 이미지를 저장해본 경험이 있습니다.`,
-    link: "https://1-blue.github.io/posts/AWS-S3-presignedURL",
+    description: `버킷을 생성하고 접근 권한을 열고 이미지를 저장할 수 있습니다.`,
+    link: "https://choigirang.github.io/posts/Node-S3-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%97%85%EB%A1%9C%EB%93%9C/",
   },
 ];
 
 /** 2023/08/14 - 사용 가능한 BE측 기술들 - by 1-blue */
 export const etcSkills: MySkills[] = [
   {
-    name: "nestjs",
-    color: "#E0234E",
-    displayName: "Nest.js",
-    description: `공식문서와 각 블로그를 읽으면서 서버를 구성해본 경험이 있습니다.`,
+    name: "mui",
+    color: "#027FFE",
+    displayName: "Material-UI",
+    description: `스타일링 및 애니메이션 구현을 위해 사용하였으며, 모드에 따른 테마 설정, 컴포넌트 커스텀을 해본 경험이 있습니다.`,
   },
   {
-    name: "amazonec2",
-    color: "#FF9900",
-    displayName: "AWS-EC2",
-    description: `개인 프로젝트를 배포할 때 사용해본 경험이 있습니다.`,
+    name: "framer",
+    color: "#027FFE",
+    displayName: "Framer-motion",
+    description: `스타일링 및 애니메이션 구현을 위해 사용해본 경험이 있습니다.`,
   },
   {
     name: "jsonwebtokens",
     color: "#000000",
     displayName: "JWT",
     description: `AccessToken과 RefreshToken을 직접 만들어서 인증을 처리하는 로직을 구현해본 경험이 있습니다.`,
-  },
-  {
-    name: "nginx",
-    color: "#009639",
-    displayName: "Nginx",
-    description: `Nginx와 Certbot을 이용해서 SSL 인증서를 발급받아서 적용해본 경험이 있습니다.`,
-    link: "https://1-blue.github.io/posts/deploy",
-  },
-  {
-    name: "recoil",
-    color: "#3578E5",
-    displayName: "Recoil",
-    description: `간단한 상태관리를 위해 사용해본 경험이 있습니다.`,
-  },
-  {
-    name: "mysql",
-    color: "#4479A1",
-    displayName: "Mysql",
-    description: `깊은 개념에 대한 지식은 부족하고 간단한 데이터 CRUD를 위한 SQL을 작성할 수 있습니다.`,
-    link: "https://velog.io/@1-blue/mysql",
   },
 ];
