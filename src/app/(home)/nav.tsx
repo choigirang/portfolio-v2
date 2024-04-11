f0ea95735805c87c96729b3fff73ca619c7c6db3
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-11 19:53:22 +0900

    chore: add bg when scroll

--- src/app/(home)/nav.tsx
+++ src/app/(home)/nav.tsx
@@ -3,6 +3,8 @@ import Category from "./category";
 
 /** 2024/04/04 - 레이아웃 header 컴포넌트 */
 export default function Header() {
+  /** 스크롤 시 bg */
+
   return (
     <nav className="nav">
       <div className="container h-full flex justify-between items-center pt-0">
