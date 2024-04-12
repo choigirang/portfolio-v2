1ab898b99c9498324da77378a8ec8a20ac375c79
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-12 17:51:54 +0900

    feat: detail description component 분리 (server)

--- src/app/(projects)/detail.tsx
+++ src/app/(projects)/detail.tsx
@@ -2,5 +2,9 @@ import { MyProject } from "@/assets/projects";
 
 /** 2024/04/11 - 프로젝트 상세 설명 팝업 */
 export default function Detail(data: MyProject) {
-  return <div>1</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <div id="summary" className="flex flex-col bg-gray-200"></div>
+    </div>
+  );
 }
