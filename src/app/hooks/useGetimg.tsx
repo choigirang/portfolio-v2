62f087d1b5980c10bb6eb27a8621b2cd345a909e
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-12 17:52:36 +0900

    feat: s3 에서 이미지 받아올 hook

---
+++ src/app/hooks/useGetimg.tsx
@@ -0,0 +1,32 @@
+import AWS from "aws-sdk";
+
+const ACCESS_ID = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID;
+const ACCESS_KEY = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY;
+const region = "ap-northeast-2";
+const bucketName = "choigirang-portfolio";
+
+export default async function useGetimg(folder: string) {
+  AWS.config.update({
+    accessKeyId: ACCESS_ID,
+    secretAccessKey: ACCESS_KEY,
+    region,
+  });
+
+  const s3 = new AWS.S3();
+
+  try {
+    const data = await s3
+      .listObjectsV2({ Bucket: bucketName, Prefix: folder })
+      .promise();
+
+    if (data.Contents) {
+      const urls = data.Contents.map((obj) => {
+        return `https://${bucketName}.s3.amazonaws.com/${obj.Key}`;
+      });
+
+      return urls.slice(1);
+    }
+  } catch (error) {
+    throw new Error(`Error listing objects: ${error}`);
+  }
+}
