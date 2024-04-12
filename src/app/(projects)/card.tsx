7d925f0ce0242d5de6f6b619e7c10e8faf4a1ebc
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-12 17:51:35 +0900

    feat: card component 분리 (client)

--- src/app/(projects)/card.tsx
+++ src/app/(projects)/card.tsx
@@ -1,22 +1,17 @@
-import Image from "next/image";
 import Detail from "./detail";
-import { MyProject, S3_URL } from "@/assets/projects";
+import { MyProject } from "@/assets/projects";
+import ImgSlide from "./imgSlide";
 
 /** 2024/04/10 - project card */
 export default function Card(data: MyProject) {
   return (
     <>
-      <li className="w-full h-[500px] grid grid-cols-projectDetail shadow-ml bg-yellow-400 rounded-md p-4">
-        <div className="relative">
-          <Image
-            src={S3_URL + data.thumbnail}
-            alt="card img"
-            fill
-            objectFit="contain"
-            placeholder="blur"
-            blurDataURL="/gif/spinner.gif"
-            className="rounded-md transition-custom"
-          />
+      <li className="w-full h-[500px] grid grid-cols-projectDetail shadow-ml bg-gray-100 rounded-2xl p-4">
+        <div className="relative flex flex-col justify-between gap-2">
+          <span className="px-4 py-2 rounded-lg bg-yellow-400">
+            {data.title}
+          </span>
+          <ImgSlide {...data} />
         </div>
         <Detail {...data} />
       </li>
