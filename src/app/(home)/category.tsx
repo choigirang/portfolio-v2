944228288df5af897b4797a75fbffeb06ac71e8f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-14 19:39:10 +0900

    feat: mobile 화면의 nav

--- src/app/(home)/category.tsx
+++ src/app/(home)/category.tsx
@@ -3,29 +3,83 @@
 import Link from "next/link";
 import { twMerge } from "tailwind-merge";
 import useScroll from "../hooks/useScroll";
+import { useState } from "react";
+import { ArrowRightCircleIcon, Bars2Icon } from "@heroicons/react/16/solid";
+import useSize from "../hooks/useSize";
 
 export default function Category() {
+  const [showNav, setShowNav] = useState(false);
+  const { isMobile, setMobile } = useSize();
+
   const onScroll = (id: string) => {
     useScroll().scrollToPage(id);
   };
 
+  /** 모바일 사이즈 */
+
+  const handleNav = () => {
+    setShowNav((prev) => !prev);
+  };
+
+  const getStyles = () => {
+    const styles = {
+      ...(isMobile && {
+        width: "70vw",
+        height: "100vh",
+        position: "absolute" as const,
+        top: 0,
+        flexDirection: "column" as const,
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 24,
+        color: "#eab308",
+        backgroundColor: "#f5f5f5",
+      }),
+      right: showNav ? "0" : "-100%",
+    };
+
+    return styles;
+  };
+
   return (
-    <ul className="flex gap-10">
-      <li onClick={() => onScroll("about")} className={twMerge("button-hover")}>
-        <Link href="/#about">About</Link>
-      </li>
-      <li
-        onClick={() => onScroll("skills")}
-        className={twMerge("button-hover")}
-      >
-        <Link href="/#skills">Skills</Link>
-      </li>
-      <li
-        onClick={() => onScroll("projects")}
-        className={twMerge("button-hover")}
-      >
-        <Link href="/#projects">Projects</Link>
-      </li>
-    </ul>
+    <>
+      {isMobile && !showNav && (
+        <Bars2Icon
+          width={32}
+          height={32}
+          onClick={handleNav}
+          className="hover:cursor-pointer hover:text-yellow-400"
+        />
+      )}
+      <ul className="flex gap-10 transition-custom" style={getStyles()}>
+        <li
+          onClick={() => onScroll("about")}
+          className={twMerge("button-hover")}
+        >
+          <Link href="/#about">About</Link>
+        </li>
+        <li
+          onClick={() => onScroll("skills")}
+          className={twMerge("button-hover")}
+        >
+          <Link href="/#skills">Skills</Link>
+        </li>
+        <li
+          onClick={() => onScroll("projects")}
+          className={twMerge("button-hover")}
+        >
+          <Link href="/#projects">Projects</Link>
+        </li>
+        {isMobile && (
+          <ArrowRightCircleIcon
+            className="absolute top-1/2 left-[10%] -translate-x-1/2 hover:text-yellow-600 hover:cursor-pointer"
+            width={32}
+            height={32}
+            onClick={handleNav}
+            onTouchStart={handleNav}
+          />
+        )}
+      </ul>
+    </>
   );
 }
