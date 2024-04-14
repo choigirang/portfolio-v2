ed6a835bc825360a5fa413f5a71f7515c9244280
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-14 19:43:40 +0900

    style: max 640px gird template => flex

--- src/app/(projects)/card.tsx
+++ src/app/(projects)/card.tsx
@@ -6,7 +6,7 @@ import ImgSlide from "./imgSlide";
 export default function Card(data: MyProject) {
   return (
     <>
-      <li className="w-full h-[500px] grid grid-cols-projectDetail shadow-ml bg-gray-100 rounded-2xl p-4">
+      <li className="grid grid-cols-projectDetail items-center w-full gap-4 shadow-ml bg-gray-100 rounded-2xl p-4 sm:flex sm:flex-col">
         <div className="relative flex flex-col justify-between gap-2">
           <span className="px-4 py-2 rounded-lg bg-yellow-400">
             {data.title}
