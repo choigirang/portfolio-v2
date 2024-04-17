9d18461c0e080052cefcd37d34075db5b6378402
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-17 16:44:42 +0900

    style

--- src/app/(projects)/card.tsx
+++ src/app/(projects)/card.tsx
@@ -7,8 +7,8 @@ export default function Card(data: MyProject) {
   return (
     <>
       <li className="grid grid-cols-projectDetail items-center w-full gap-4 shadow-ml bg-gray-100 rounded-2xl p-4 sm:flex sm:flex-col md:flex md:flex-col">
-        <div className="relative flex flex-col justify-between gap-2">
-          <span className="px-4 py-2 rounded-lg bg-yellow-400">
+        <div className="h-full flex flex-col justify-between items-center gap-2">
+          <span className="w-full max-w-[500px] px-4 py-2 rounded-lg bg-yellow-400">
             {data.title}
           </span>
           <ImgSlide {...data} />
