da283482502bf2191b18b05fc7fb41da637362f4
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-27 18:56:18 +0900

    feat: change folder by repo name change

--- src/hooks/useGetimg.tsx
+++ src/hooks/useGetimg.tsx
@@ -17,7 +17,10 @@ export default async function useGetimg(folder: string) {
 
   try {
     const data = await s3
-      .listObjectsV2({ Bucket: bucketName, Prefix: `${folder}/` })
+      .listObjectsV2({
+        Bucket: bucketName,
+        Prefix: `${folder === "windows95" ? "pokemon" : folder}/`,
+      })
       .promise();
 
     if (data.Contents) {
