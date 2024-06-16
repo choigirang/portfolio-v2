c9e81dcd9f73ed5f9c95e4050119d94a82b90096
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-16 19:10:27 +0900

    Update card.tsx

--- src/app/(projects)/card.tsx
+++ src/app/(projects)/card.tsx
@@ -7,9 +7,7 @@ export default function Card(data: MyProject) {
   return (
     <>
       <li className="grid grid-cols-projectDetail items-center w-full gap-4 shadow-ml bg-gray-100 rounded-2xl p-4 sm:flex sm:flex-col md:flex md:flex-col">
-        <div className="h-full flex flex-col justify-between items-center gap-2">
-          <ImgSlide {...data} />
-        </div>
+        <ImgSlide {...data} />
         <Detail {...data} />
       </li>
     </>
