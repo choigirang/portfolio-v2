6c856ed4731df531be2849380e6e182a499a6fb4
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-16 22:51:44 +0900

    add project data

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -30,6 +30,81 @@ export const S3_URL =
 /** 2024/04/10 - 프로젝트 소개 */
 export const projectsData: MyProject[] = [
   {
+    period: "2024.06 ~ 2024.06",
+    title: "front를 위한 dev quiz",
+    name: "quiz",
+    displayName: "front를 위한 dev quiz",
+    role: "개인 프로젝트",
+    summary:
+      "개발 관련 퀴즈를 풀고 사용자의 랭킹을 확인할 수 있는 프로젝트입니다.",
+    front: [
+      { name: "typescript", displayName: "Type-script", color: "#387BC8" },
+      { name: "react", displayName: "React", color: "#000000" },
+      { name: "redux", displayName: "Redux", color: "#764ABC" },
+      { name: "reactquery", displayName: "React-Query", color: "#FF4154" },
+      {
+        name: "tailwindcss",
+        color: "#06B6D4",
+        displayName: "TailwindCss",
+      },
+      { name: "firebase", displayName: "Firebase", color: "#DC2C01" },
+    ],
+    back: [],
+    github: "https://github.com/choigirang/quiz",
+    link: "https://www.dev-quiz.store/",
+    description: [
+      {
+        title:
+          "*Router를 학습하고 사용자의 선택에 따른 빠른 구동을 위해 React를 사용*하였습니다.",
+        detail: [
+          "React-router를 사용하여 페이지 전환을 구현하였으며, 선택한 데이터에 대한 페이지 전환을 위해 중첩 라우터를 사용하였습니다.",
+          "데이터를 불러오는 시간과 경우를 축소하기 위해 동적 import를 사용하였습니다.",
+          "문제를 풀고 종합 해설지를 볼 때, 랭킹과 사용자 분포도에 대한 차트를 볼 때 공통적으로 사용되는 코드를 줄이기 위해 재사용 가능한 컴포넌트를 작성하였습니다.",
+          "useMemo, useCallback, React.memo 를 사용하여 렌더링을 최소화하였습니다.",
+          "데이터 선택, 문제 제출 등에 대한 hook을 작성하여 코드를 최소화하였습니다.",
+          "CSR의 SEO 취약점을 해결하기 위해 sitemap 생성기를 설정하여 각 페이지에 대한 정보를 설정하였습니다.",
+        ],
+      },
+      {
+        title:
+          "*타입 에러를 통한 프로젝트 안정성을 높이기 위해 typescript를 사용하였습니다.*",
+        detail: [
+          "비동기 데이터 응답값에 대한 타입을 작성하였습니다.",
+          "재사용 컴포넌트의 props에 대한 타입 가드를 사용하여 작성하였습니다.",
+        ],
+      },
+      {
+        title:
+          "*RTK, redux-persist를 사용하여 페이지 전환, 새로고침 시 데이터를 유지합니다.*",
+        detail: [
+          "퀴즈를 풀다가 랭킹을 보거나, 해설집을 보다가 퀴즈를 푸는 등의 페이지 전환이 일어나거나 새로고침 시에도 이전에 선택했던 데이터와 풀고 있던 퀴즈를 유지할 수 있습니다.",
+          "문제를 동적 import로 가져온 후, 무작위로 사용자에게 제공되기 때문에 초기에 저장된 문제, 제출 완료한 문제를 저장하여 사용합니다.",
+        ],
+      },
+      {
+        title:
+          "*react-query를 사용하여 사용자에 대한 정보와 랭킹 등 API요청을 최소화하였습니다.*",
+        detail: [
+          "API 요청에 대한 custom hook을 만들어 반복되는 로직을 최소화하였습니다.",
+          "hook 사용 시, 선택한 값이 없음에도 동작하는 것을 방지하기 위해 최초 실행을 방지하고, refetch를 통해 데이터를 받아옵니다.",
+          "react-query의 캐시를 사용하여 이전에 불러온 데이터가 있을 시 이를 활용합니다.",
+        ],
+      },
+      {
+        title:
+          "다양한 기기의 접근성을 높이기 위해 *반응형 웹으로 작성*하였습니다.",
+        detail: [
+          "화면 크기에 따라 레이아웃과 스타일을 변경하여 사용자가 다양한 기기에서 접근이 가능하고, 기기 별 편의성을 높였습니다.",
+        ],
+      },
+      {
+        title:
+          "*firebase storage에 값을 저장하고 사용하며 API 요청과 response에 대해 학습*하였습니다.",
+        detail: [],
+      },
+    ],
+  },
+  {
     period: "2024.04 ~ 2024.04",
     title: "Portfolio - v2",
     name: "portfolioV2",
@@ -71,6 +146,7 @@ export const projectsData: MyProject[] = [
           "필요에 따라 *클라이언트 / 서버 컴포넌트를 분리*하여 작성하였습니다.",
         detail: [
           "data를 props로 활용하고, children을 사용하여 클라이언트 컴포넌트 안에서 서버 컴포넌트를 사용하였습니다.",
+          "클라이언트 컴포넌트에서 useMemo, useCallback, React.memo 를 사용하여 최적화 하였습니다.",
           "아코디언, 툴팁 등의 애니메이션이 필요한 컴포넌트를 분리하였습니다.",
         ],
       },
@@ -279,12 +355,14 @@ export const projectsData: MyProject[] = [
           "*타입에러를 통한 프로젝트의 안정성을 높이기 위해 typescript로 제작*하였습니다.",
         detail: [
           "키보드, 마우스 이벤트에 대한 타입을 작성하여 사용할 수 있습니다.",
+          "데이터 타입을 정의하고 특정 데이터에 대해 유틸리티 타입을 사용하였습니다.",
         ],
       },
       {
         title: "*recoil을 사용하여 전역 상태를 관리하였습니다.*",
         detail: [
-          "사용자가 선택한 아이콘 및 데이터에 대한 상태값을 정의하여 변경 및 사용할 수 있습니다.",
+          "윈도우의 특성을 모방하기 위해, 메뉴가 열려있을 시 바탕화면을 클릭했을 때 메뉴가 닫히거나 윈도우 프로그램이 열려있을 시 바탕화면을 클릭하여 focus를 변경하는 등 발생하는 이벤트에 따른 상태값을 전역적으로 변경하는 데 사용하였습니다.",
+          "사용자가 선택한 아이콘 및 데이터에 대한 상태값을 정의하여 변경 및 사용하였습니다.",
         ],
       },
       {
@@ -353,7 +431,7 @@ export const projectsData: MyProject[] = [
           "*Next.js에 최적화된 스타일 적용을 위해 tailwind*로 마이그레이션을 진행하였습니다.",
         detail: [
           "런타임에서 적용되는 styled-component를 제거하고 빌드 타임에 적용되는 tailwind를 선택하여 SSR 시 별도의 설정 없이 스타일이 적용될 수 있도록 하였습니다.",
-          "가독성을 높이기 위해 반복되는 스타일을 미리 선언하여 적용하였습니다.",
+          "가독성을 높이기 위해 반복되는 스타일을 선언하여 적용하거나 함수에 전달되는 변수에 의해 스타일을 적용하였습니다.",
           "커스텀 테마를 사용하여 스타일을 적용할 수 있습니다.",
         ],
       },
